8e099f7f8f95a38af05f0eb02a01d0dd202cb97d
nandodoria <nandodoria@hotmail.ca>
2019-04-19 19:40:32 -0400

    adding colour, responsive design, and font choices

--- admin/index.php
+++ admin/index.php
@@ -7,6 +7,8 @@
 <html>
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" media="screen" href="../css/main.css"/>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
 	<title>Admin Panel</title>
 </head>
 
@@ -16,23 +18,21 @@
 	<?php include('../templates/dashboard.html'); ?>
 	<!-- Dashboard Nav End-->
 
-	
+	<div class="welcome">
 	<h1>Admin Dashboard</h1>
 	<h3>Welcome <?php echo $_SESSION['user_name'];?></h3>
-	<p>This is the admin dashboard page</p>
+	<p>This is the admin dashboard page. Control admin users and add/edit Sport Chek Products.</p>
 
 	<?php if(!empty($message)):?>
 		<p><?php echo $message;?></p>
 	<?php endif;?>
+	</div>
 
 	<nav>
-		<ul>
+		<ul class="mainNav">
 			<li><a href="admin_createuser.php">Create User</a></li>
 			<li><a href="admin_edituser.php">Edit User</a></li>
 			<li><a href="admin_deleteuser.php">Delete User</a></li>
-		</ul>
-
-		<ul>
 			<li><a href="admin_addproduct.php">Add A Product</a></li>
 			<li><a href="admin_editproduct.php">Edit A Product</a></li>
 		</ul>
--- details.php
+++ details.php
@@ -13,21 +13,36 @@ if(isset($_GET['id'])){
 <html>
 <head>
 	<meta charset='utf-8'>
+	<link rel="stylesheet" type="text/css" media="screen" href="css/main.css"/>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
 	<title>Sport Chek</title>
 </head>
 <body>
 	<!-- Header -->
-		<?php include('templates/header.html'); ?>
+	<div class="navstyle">
+	<img class="logo" src="images/logo.png" alt="logo">
+	<div class="navright">
+	<?php include('templates/header.html'); ?>
+	<!-- <form method="POST" action="admin/search.php" id="searchform"> 
+		<input type="text" name="search"> 
+		<input type="submit" name="submit" value="Search"> 
+	</form> -->
+	</div>
+	</div>
 	<!-- Header End-->
 
-	<a href="index.php">Go Back</a>
+	<a class="backbtn" href="index.php">GO BACK</a>
 
 	<div>
 	<?php while($row = $results->fetch(PDO::FETCH_ASSOC)):?>
-		<img src="images/<?php echo $row['prod_pic']; ?>">
-		<h2><?php echo $row['prod_name'];?></h2>
-		<p><?php echo $row['prod_text'];?></p>
-		<p>$<?php echo $row['prod_price'];?></p>
+		<div class="detailsall">
+		<img class="detailspic" src="images/<?php echo $row['prod_pic']; ?>">
+		<div class="detailstext">
+		<h2 class="detailstitle"><?php echo $row['prod_name'];?></h2>
+		<p class="detailsdesc"><?php echo $row['prod_text'];?></p>
+		<p class="detailsprice">$<?php echo $row['prod_price'];?></p>
+		</div>
+		</div>
 	<?php endwhile;?>
 	</div>
 
